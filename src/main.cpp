646da8f6e506e6569a2ceb77432f3402ae831896
BigMel <melvillemaeda@gmail.com>
2026-01-24 16:09:12 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+const int light = 3;
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  printf("Hello World\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+
+  return x + y;
+}
